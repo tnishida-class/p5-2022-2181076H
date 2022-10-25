31a5f61521612c7f663709e3620eb376dd855a5e
2181076H <114969043+2181076H@users.noreply.github.com>
2022-10-25 09:50:39 +0900

    change section 4-2

--- section4-2/sketch.js
+++ section4-2/sketch.js
@@ -8,11 +8,13 @@ function draw(){
   background(160, 192, 255);
   for(let i = 0; i < balls.length; i++){ // すべてのボールを描画し、速度の分だけ移動させる
     let b = balls[i];
-    ellipse(b.x, b.y, b.size);
+    ellipse(b.x, b.y,b.size);
     b.x += b.vx;
-    b.y += b.vy;
-    size=random(20,100);
-    b.size =size; // 60以上100未満のランダムな数を代入
+    b.y += b.vy; 
+    
+
+    
+  
   }
 }
 
@@ -20,7 +22,7 @@ function mouseDragged(){ // ドラッグされたらボールを増やす
   const dx = mouseX - pmouseX;
   const dy = mouseY - pmouseY;
   if(mag(dx, dy) > 5){ // mag(x,y) はベクトル(x,y)の長さ
-    const b = { x: mouseX, y: mouseY, size: 20, vx: dx, vy: dy };
+    const b = { x: mouseX, y: mouseY, size: random(20,100), vx: dx, vy: dy };
     balls.push(b);
   }
 }
