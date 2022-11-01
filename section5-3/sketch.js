d30d3e919673760b985e58b6a6362823df37270e
2181076H <114969043+2181076H@users.noreply.github.com>
2022-11-01 12:05:56 +0900

    change section 5-3

--- section5-3/sketch.js
+++ section5-3/sketch.js
@@ -1,17 +1,23 @@
 // テキスト「関数を作る(2) 結果を戻す関数」～「総仕上げ：カレンダーを描画しよう」
 function setup(){
   createCanvas(200, 200);
-  calendar(2019, 10);
+  //calendar(2019, 10);
 
   // isLeapYear の動作確認のため console に出力しています
-  for(let i = 2000; i <= 2100; i++){
-    if(isLeapYear(i)){
-      console.log(i + "年はうるう年です");
-    }
-    else{
-      console.log(i + "年はうるう年ではありません");
-    }
-  }
+  //for(let i = 2000; i <= 2100; i++){
+    //if(isLeapYear(i)){
+      //console.log(i + "年はうるう年です");
+    //}
+    //else{
+      //console.log(i + "年はうるう年ではありません");
+    //}
+  //}
+
+  //daysInYear(2022);
+  //dayOfYear(2022, 1, 1);
+  dayOfWeek(1981,7,17);
+  dayOfWeek(2001,6,25);
+  dayOfWeek(2022,11,1);
 }
 
 function calendar(y, m){
@@ -26,7 +32,13 @@ function isLeapYear(y){
 }
 
 function daysInYear(y){
-  // BLANK[1]
+  if(isLeapYear(y)){
+    return 366;
+    }
+    else{
+    return 365;
+    }
+    
 }
 
 function daysInMonth(y, m){
@@ -46,11 +58,31 @@ function dayOfYear(y, m, d){
   for(let i = 1; i < m; i++){
     count += daysInMonth(y, i);
   }
-  return count + d;
+  //return count + d;
+  console.log(count+d);
 }
 
 function dayOfWeek(y, m, d){
-  // BLANK[2]
+  let y_count =0;
+  for(let i=1970;i<y;i++){
+    y_count += daysInYear(i);
+  }
+
+  //console.log(y_count);
+
+  let m_count = 0;
+  for(let i = 1; i < m; i++){
+    m_count += daysInMonth(y, i);
+  }
+  //return count + d;
+  //console.log(m_count+d-1);
+  //console.log(y_count+m_count+d-1);
+  let day=y_count+m_count+d-1;
+  //console.log(day);
+  //console.log(day%7);
+  console.log((4+day%7)%7);
+  
+  
 }
 
 function dayOfWeekAsString(dow){
